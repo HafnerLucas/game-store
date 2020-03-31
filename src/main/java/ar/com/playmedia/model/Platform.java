08ed5417cb04ce16b33c85da880987ff29f9beb3
HafnerLucas <lucas.ezequiel.hafner@outlook.com>
2020-03-31 16:37:42 -0300

    agrego modelo y primer vista

---
+++ src/main/java/ar/com/playmedia/model/Platform.java
@@ -0,0 +1,34 @@
+package ar.com.playmedia.model;
+
+public class Platform {
+    private Integer id;
+    private String description;
+
+    public Platform()
+
+    public Platform(
+        Integer id,
+        String description
+    ){
+        this.id = id;
+		this.description = description;
+    }
+
+    public Integer getId() {
+		return id;
+	}
+
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+    public String getDescription() {
+		return description;
+	}
+
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
