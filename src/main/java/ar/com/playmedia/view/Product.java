ac4b4ad6a88cb6efb2492f4f7af96d25e358c40c
HafnerLucas <lucas.ezequiel.hafner@outlook.com>
2020-04-05 13:50:45 -0300

    modifico metodos geter y setter

--- src/main/java/ar/com/playmedia/view/Product.java
+++ src/main/java/ar/com/playmedia/view/Product.java
@@ -3,6 +3,7 @@ package ar.com.playmedia.view;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
 public class Product {
 	private Scanner keyboard;
 	private ar.com.playmedia.controller.Product handler;
@@ -21,7 +22,7 @@ public class Product {
 		Integer option = -1;
 
         while(option != 0) {
-			clearScreen();
+			 clearScreen();
 
 			System.out.println("Productos:");
 			System.out.println("=========");
@@ -32,7 +33,7 @@ public class Product {
 			System.out.println();
 			System.out.println("	0) Salir");
 			System.out.println();
-			System.out.print("Opcion: ");
+			System.out.println("Opcion: ");
 
 			option = Integer.parseInt(keyboard.nextLine());
 
@@ -57,27 +58,28 @@ public class Product {
 
 		while(other != 0) {
 			clearScreen();
-			System.out.println("Alta de Producto:");
+			System.out.println("Alta de Producto: german");
 			System.out.println("==== == =========");
-			System.out.print("	Ingrese una breve descripcion del producto: ");
+			System.out.println();
+			System.out.println("Ingrese una breve descripcion del producto: ");
 			product.setDescription(keyboard.nextLine());
 
-			System.out.print("	Ingrese precio del producto: ");
+			System.out.println("	Ingrese precio del producto: ");
 			product.setPrice(Float.parseFloat(keyboard.nextLine()));
 
-			System.out.print("	Ingrese la cantidad del producto: ");
+			System.out.println("	Ingrese la cantidad del producto: ");
 			product.setQuantity(Integer.parseInt(keyboard.nextLine()));
 
-			System.out.print("	Ingrese categoria del producto: ");
-			System.out.print("	1) Consola");
-			System.out.print("	2) Accesorios");
-			System.out.print("	3) Juegos");
+			System.out.println("	Ingrese categoria del producto: ");
+			System.out.println("	1) Consola");
+			System.out.println("	2) Accesorios");
+			System.out.println("	3) Juegos");
 			product.setCategory(Integer.parseInt(keyboard.nextLine()));
 
-			System.out.print("	Ingrese categoria del producto: ");
-			System.out.print("	1) Nintendo");
-			System.out.print("	2) Playstation");
-			System.out.print("	3) Xbox");
+			System.out.println("	Ingrese categoria del producto: ");
+			System.out.println("	1) Nintendo");
+			System.out.println("	2) Playstation");
+			System.out.println("	3) Xbox");
 			product.setPlatform(Integer.parseInt(keyboard.nextLine()));
 
 			handler.connect();
@@ -85,7 +87,7 @@ public class Product {
 			handler.disconnect();
 
 			System.out.println();
-			System.out.print("Agregar otro contacto? (0 no / 1 si): ");
+			System.out.println("Agregar otro contacto? (0 no / 1 si): ");
 			other = Integer.parseInt(keyboard.nextLine());
 		}
 	}
